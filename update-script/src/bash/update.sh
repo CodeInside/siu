9001e30c1b44847bd4a9d393ac1515731bdd8cc6
Xeodon <xeodon@gmail.com>
2014-02-14 17:27:32 +0400

    Re #1095 убрать универсальный сервис из обновления

--- update-script/src/bash/update.sh
+++ update-script/src/bash/update.sh
@@ -6,19 +6,17 @@
 #!/bin/sh -e
 
 # что идентифицирует предыдущий релиз
-RED_LINE="web-client-1.0.3"
+RED_LINE="web-client-1.0.3.1"
 
 # в порядке удаления
 TO_REMOVE="
-web-client-1.0.3
+web-client-1.0.3.1
 gws-api-1.0.5
-gws-s-oep-declarer-1.0.2
 "
 
 # в порядке установки
 TO_INSTALL="
 gws-api-1.0.6
-gws-s-oep-declarer-1.0.3
 web-client-1.0.4
 "
 
