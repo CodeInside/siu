a3fa140b029d9eeda116df064fc9509e1bb5930f
alexey <lm231290@gmail.com>
2014-03-19 16:22:08 +0400

    Re #1126 - Очистка бд на hudson

---
+++ web-client/src/test/resources/cleandb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+	psql gses_admin -U gses -h 46.4.98.139 > clean.sql <<EOF
+copy (
+	select 'drop table ' || tablename || ' cascade;'
+	FROM pg_tables WHERE schemaname = 'public'
+) to STDOUT;
+copy (
+	select 'drop sequence ' || c.relname || ' cascade;'
+	FROM pg_catalog.pg_class AS c
+	LEFT JOIN pg_catalog.pg_namespace AS n ON n.oid = c.relnamespace WHERE relkind = 'S' AND n.nspname = 'public'
+) to STDOUT;
+EOF
+	psql gses_admin -U gses -h 46.4.98.139 < clean.sql
