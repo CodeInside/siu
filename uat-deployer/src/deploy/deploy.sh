ecebd4e6ae1605361c5420ed69e4efab25d82a23
alexey <lm231290@gmail.com>
2014-03-19 16:54:41 +0400

    Re #1126 - Очистка бд на hudson

--- uat-deployer/src/deploy/deploy.sh
+++ uat-deployer/src/deploy/deploy.sh
@@ -41,7 +41,7 @@ gws-log-1.0.1
 gws-p-registry-hc-1.0.1
 gws-p-registry-api-1.0.1
 gws-c-oep-declarer-1.0.2
-gws-s-oep-declarer-1.0.3
+gws-s-oep-declarer-1.0.2
 gws-api-1.0.5
 gws-api-1.0.6
 gws-api-1.0.7
@@ -65,7 +65,7 @@ gws-p-registry-hc-1.0.1
 gws-p-adapter-1.0.3
 gws-p-router-1.0.4
 gws-s-oep-dict-1.0.1
-gws-s-oep-declarer-1.0.3
+gws-s-oep-declarer-1.0.2
 gws-c-oep-declarer-1.0.2
 web-client-1.0.5
 "
