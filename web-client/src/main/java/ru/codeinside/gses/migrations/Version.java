a588453b9fe566b29ed9fcf54afe830b0fed41d7
o.hlinov <o.hlinov@gmail.com>
2014-05-30 11:53:00 +0400

    re #1168 Решил проблему с миграцией БД

--- web-client/src/main/java/ru/codeinside/gses/migrations/Version.java
+++ web-client/src/main/java/ru/codeinside/gses/migrations/Version.java
@@ -1,15 +1,15 @@
 /*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- * Copyright (c) 2013, MPL CodeInside http://codeinside.ru
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ * Copyright (c) 2014, MPL CodeInside http://codeinside.ru
  */
 
 package ru.codeinside.gses.migrations;
 
 final public class Version {
 
-  final public static String DB_VERSION = "1.6";
+  final public static String DB_VERSION = "1.7";
 
   private Version() {
   }
