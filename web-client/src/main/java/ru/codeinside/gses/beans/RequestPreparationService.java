9ee36de861eb3cda9f6c2c508ae08f5604241c6a
Evgin Goncharov <evgin.goncharov@gmail.com>
2014-10-23 16:31:22 +0400

    разбор zip файла с заявлением

--- web-client/src/main/java/ru/codeinside/gses/beans/RequestPreparationService.java
+++ web-client/src/main/java/ru/codeinside/gses/beans/RequestPreparationService.java
@@ -1,19 +1,33 @@
 package ru.codeinside.gses.beans;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.Enumeration;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
+
+import net.mobidom.bp.beans.Request;
 
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.delegate.DelegateExecution;
 
 import ru.codeinside.adm.AdminService;
 import ru.codeinside.gws.api.Enclosure;
+import commons.Streams;
 
 @Named("requestPreparationService")
 @Singleton
@@ -42,21 +56,58 @@ public class RequestPreparationService {
 
     log.info("======================\n" + "prepareRequest invoked\n" + "======================");
 
-    log.fine("variables");
-    logVars(execution.getVariables());
+    try {
+      ActivitiExchangeContext aeCtx = new ActivitiExchangeContext(execution);
 
-    log.fine("local variables");
-    logVars(execution.getVariablesLocal());
+      Enclosure enclosure = aeCtx.getEnclosure("attachment");
 
-    log.fine("=======");
+      byte[] zipData = enclosure.content;
 
-    log.fine("aeCtx variables");
-    ActivitiExchangeContext aeCtx = new ActivitiExchangeContext(execution);
-    Enclosure enclosure = aeCtx.getEnclosure("attachment");
-    log.fine("enclosure = " + enclosure);
+      File tmpFile = File.createTempFile("tmp", "zip");
+      FileOutputStream tmpFileOutputStream = new FileOutputStream(tmpFile);
+      tmpFileOutputStream.write(zipData);
+      tmpFileOutputStream.close();
+
+      ZipFile zipFile = new ZipFile(tmpFile);
+
+      ZipEntry entry = null;
+      Enumeration<? extends ZipEntry> entries = zipFile.entries();
+      while (entries.hasMoreElements()) {
+        entry = entries.nextElement();
+
+        log.info("entry = " + entry.getName());
+        String fileName = entry.getName();
+        if (fileName.endsWith(".xml")) {
+          log.info(fileName + " - may be file request");
+
+          String requestId = fileName.split("\\.")[0];
+          log.info("request id = " + requestId);
+
+          InputStream in = zipFile.getInputStream(entry);
+
+          JAXBContext jaxbContext = JAXBContext.newInstance("net.mobidom.bp.beans");
+          Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+          JAXBElement<Request> element = (JAXBElement<Request>) unmarshaller.unmarshal(in);
+          Request request = element.getValue();
+
+          log.info("request = " + request.getGovernmentAgencyName());
+
+          execution.createVariableLocal("request", request);
+
+          in.close();
+
+        } else {
+          log.info(fileName + " - not file request");
+        }
+      }
+
+      tmpFile.delete();
+
+    } catch (Throwable t) {
+      log.log(Level.SEVERE, "can't prepare request", t);
+    }
 
     log.fine("=======");
 
   }
-
 }
