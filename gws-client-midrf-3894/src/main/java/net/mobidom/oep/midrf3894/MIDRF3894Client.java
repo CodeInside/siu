b2335882bfc05cc3b584ba10f3f0d36654f265d4
Evgin Goncharov <evgin.goncharov@gmail.com>
2014-12-03 15:36:01 +0300

    доработка клиента midrf3894

--- gws-client-midrf-3894/src/main/java/net/mobidom/oep/midrf3894/MIDRF3894Client.java
+++ gws-client-midrf-3894/src/main/java/net/mobidom/oep/midrf3894/MIDRF3894Client.java
@@ -11,12 +11,14 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBElement;
 import javax.xml.bind.Marshaller;
 import javax.xml.namespace.QName;
+import javax.xml.transform.dom.DOMResult;
 
+import net.mobidom.bp.beans.XmlContentWrapper;
+import net.mobidom.bp.beans.Документ;
 import net.mobidom.bp.beans.request.DocumentRequest;
+import net.mobidom.bp.beans.request.DocumentRequestType;
 
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.Document;
 
 import ru.codeinside.gws.api.Client;
 import ru.codeinside.gws.api.ClientRequest;
@@ -27,7 +29,10 @@ import ru.codeinside.gws.api.Packet;
 import ru.codeinside.gws.api.Packet.Status;
 import ru.codeinside.gws.api.Packet.Type;
 import ru.codeinside.gws.api.Revision;
+import ru.gosuslugi.smev.rev111111.SmevAppData;
+import ru.kdmid.ObjectFactory;
 import ru.kdmid.SmevreqAppData;
+import ru.kdmid.SmevresAppData;
 
 public class MIDRF3894Client implements Client {
 
@@ -43,13 +48,24 @@ public class MIDRF3894Client implements Client {
     return getClass().getClassLoader().getResource("midrf3894/wsdl_1.wsdl");
   }
 
+  private DocumentRequest getDocumentRequest(ExchangeContext ctx) {
+    DocumentRequest documentRequest = (DocumentRequest) ctx.getVariable("REQUEST_OBJECT");
+    if (documentRequest == null) {
+      throw new IllegalStateException("Context have no parameter 'REQUEST_OBJECT'");
+    }
+
+    return documentRequest;
+  }
+
   @Override
   public ClientRequest createClientRequest(ExchangeContext ctx) {
     log.info("create client request");
 
-    DocumentRequest documentRequest = (DocumentRequest) ctx.getVariable("REQUEST_OBJECT");
-    if (documentRequest == null) {
-      throw new IllegalStateException("Context have no parameter 'REQUEST_OBJECT'");
+    DocumentRequest documentRequest = getDocumentRequest(ctx);
+    if (documentRequest.getRequestType() == null) {
+      documentRequest.setRequestType(DocumentRequestType.ЗАПРОС_ДОКУМЕНТА);
+    } else if (documentRequest.getRequestType() != DocumentRequestType.ЗАПРОС_ДОКУМЕНТА) {
+      throw new IllegalStateException(String.format("Unknown DocumentRequestType for client midrf3894'%s'", documentRequest.getRequestType()));
     }
 
     Packet packet = new Packet();
@@ -58,14 +74,16 @@ public class MIDRF3894Client implements Client {
     packet.exchangeType = "2";
     packet.recipient = new InfoSystem("MIDR09001", "МИД России");
     packet.status = Status.REQUEST;
-    // TODO webdom check system environment
-    packet.testMsg = "Тест";
+
+    if (documentRequest.getTestMessage() != null) {
+      packet.testMsg = documentRequest.getTestMessage();
+    }
 
     // TODO webdom разобраться как там вызывающий работает с исключениями
     ClientRequest clientRequest = null;
     try {
       clientRequest = new ClientRequest();
-      clientRequest.appData = createAppData(ctx);
+      clientRequest.appData = createAppData(documentRequest);
       clientRequest.packet = packet;
       clientRequest.service = new QName("http://www.kdmid.ru/", "Service1");
       clientRequest.port = new QName("http://www.kdmid.ru/", "TranslationSoap");
@@ -78,38 +96,33 @@ public class MIDRF3894Client implements Client {
     return clientRequest;
   }
 
-  private String createAppData(ExchangeContext ctx) throws Exception {
-    if (!ctx.getVariableNames().contains("transcrib_params")) {
-      throw new IllegalArgumentException("'transcrib_params' variable noit found in ctx");
+  private String createAppData(DocumentRequest documentRequest) throws Exception {
+
+    Map<String, Object> params = documentRequest.getRequestParams();
+    SmevreqAppData appDataParam = new SmevreqAppData();
+
+    if (params.containsKey("LAT_SURNAME")) {
+      appDataParam.setLATSURNAME((String) params.get("LAT_SURNAME"));
     }
 
-    Map<String, String> transcribParams = (Map<String, String>) ctx.getVariable("transcrib_params");
-
-    SmevreqAppData params = new SmevreqAppData();
-
-    if (transcribParams.containsKey("LAT_SURNAME")) {
-      params.setLATSURNAME(transcribParams.get("LAT_SURNAME"));
+    if (params.containsKey("LAT_FIRSTNAME")) {
+      appDataParam.setLATFIRSTNAME((String) params.get("LAT_FIRSTNAME"));
     }
 
-    if (transcribParams.containsKey("LAT_FIRSTNAME")) {
-      params.setLATFIRSTNAME(transcribParams.get("LAT_FIRSTNAME"));
-    }
-
-    if (transcribParams.containsKey("COUNTRY_CODE")) {
-      params.setCOUNTRYCODE(transcribParams.get("COUNTRY_CODE"));
+    if (params.containsKey("COUNTRY_CODE")) {
+      appDataParam.setCOUNTRYCODE((String) params.get("COUNTRY_CODE"));
     }
 
     // SmevreqAppData.class is not @javax.xml.bind.annotation.XmlRootElement.
     // creating root manually
     QName rootQName = new QName("http://www.kdmid.ru/", "Request");
-    JAXBElement<SmevreqAppData> root = new JAXBElement<SmevreqAppData>(rootQName, SmevreqAppData.class, params);
+    JAXBElement<SmevreqAppData> root = new JAXBElement<SmevreqAppData>(rootQName, SmevreqAppData.class, appDataParam);
 
     String appDataContent = null;
 
     StringWriter sw = new StringWriter();
     JAXBContext jaxbContext = JAXBContext.newInstance(SmevreqAppData.class);
     Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-    jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
     jaxbMarshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
     jaxbMarshaller.marshal(root, sw);
     appDataContent = sw.toString();
@@ -121,23 +134,48 @@ public class MIDRF3894Client implements Client {
   public void processClientResponse(ClientResponse clientResponse, ExchangeContext ctx) {
     log.info("process client response");
 
-    Element appDataElement = clientResponse.appData;
+    DocumentRequest documentRequest = getDocumentRequest(ctx);
 
-    NodeList childs = appDataElement.getChildNodes();
-    if (childs.getLength() == 0) {
-      ctx.setVariable("transcrib_request_fault", "Не удалось обработать запрос.");
-      // TODO webdom
-      return;
+    JAXBContext jaxbContext = null;
+    try {
+      jaxbContext = JAXBContext.newInstance(SmevAppData.class);
+    } catch (Exception e) {
+      throw new IllegalStateException(e);
     }
 
-    for (int i = 0; i < childs.getLength(); i++) {
-      Node node = childs.item(i);
-      String name = node.getLocalName();
-      if (name.equals("Response")) {
-        ctx.setVariable("transcrib_request_result", node);
+    SmevresAppData response = null;
+    try {
+      JAXBElement<SmevAppData> appDataElement = jaxbContext.createUnmarshaller().unmarshal(clientResponse.appData, SmevAppData.class);
+      SmevAppData appData = appDataElement.getValue();
+      if (appData.getResponse() == null) {
+        throw new Exception("unable to parse response appData");
       }
+      response = appData.getResponse();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+    int errorCode = response.getERRCODE();
+    if (errorCode != 0) {
+      documentRequest.setFault(String.format("ERROR_CODE: '%s'\n ERROR_DESCR: '%s'", response.getERRCODE(), response.getERRDESCRIPTION()));
+    } else {
+      Документ doc = new Документ();
+      XmlContentWrapper xmlContentWrapper = new XmlContentWrapper();
+
+      DOMResult domResult = new DOMResult();
+      try {
+        JAXBElement<SmevresAppData> responseElement = new ObjectFactory().createResponse(response);
+        jaxbContext.createMarshaller().marshal(responseElement, domResult);
+      } catch (Exception e) {
+        throw new IllegalStateException(e);
+      }
+
+      xmlContentWrapper.setXmlContent(((Document) domResult.getNode()).getDocumentElement());
+      doc.setXmlContent(xmlContentWrapper);
+      doc.setDocumentType(documentRequest.getType());
+      documentRequest.setДокумент(doc);
+      documentRequest.setReady(true);
     }
 
   }
-
 }
---
+++ gws-client-midrf-3894/src/test/java/net/mobidom/oep/midrf3894/MIDRF3894ClientTest.java
@@ -0,0 +1,62 @@
+package net.mobidom.oep.midrf3894;
+
+import java.util.logging.Logger;
+
+import net.mobidom.bp.beans.request.DocumentRequest;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import ru.codeinside.gws.api.ClientRequest;
+import ru.codeinside.gws.api.ClientResponse;
+import ru.codeinside.gws.api.ExchangeContext;
+import ru.codeinside.gws.api.InfoSystem;
+import ru.codeinside.gws.core.cproto.ClientRev111111;
+import ru.codeinside.gws.crypto.cryptopro.CryptoProvider;
+import ru.codeinside.gws.wsdl.ServiceDefinitionParser;
+
+import com.sun.xml.ws.transport.http.client.HttpTransportPipe;
+
+public class MIDRF3894ClientTest {
+
+  static Logger log = Logger.getLogger(MIDRF3894ClientTest.class.getName());
+
+  @Ignore
+  @Test
+  public void serviceInstanceInvoke() throws Exception {
+    InfoSystem pnzr01581 = new InfoSystem("PNZR01581", "Комплексная система предоставления государственных и муниципальных услуг Пензенской области");
+    // String SERVICE_ADDRESS =
+    // "http://smev-mvf.test.gosuslugi.ru:7777/gateway/services/SID0003634/1.00";
+    String SERVICE_ADDRESS = "http://213.208.170.40:9081/Translationservice.asmx";
+
+    CryptoProvider cryptoProvider = new CryptoProvider();
+    ServiceDefinitionParser definitionParser = new ServiceDefinitionParser();
+    MIDRF3894Client client = new MIDRF3894Client();
+
+    ClientRev111111 rev111111 = new ClientRev111111(definitionParser, cryptoProvider);
+
+    DocumentRequest documentRequest = new DocumentRequest();
+    documentRequest.addRequestParam("LAT_SURNAME", "LABBEE");
+    documentRequest.addRequestParam("LAT_FIRSTNAME", "GABRIELLE CECILIA");
+    documentRequest.addRequestParam("COUNTRY_CODE", "USA");
+    documentRequest.setTestMessage("Test");
+
+    ExchangeContext ctx = new DummyContext();
+    ctx.setVariable("REQUEST_OBJECT", documentRequest);
+
+    ClientRequest request = client.createClientRequest(ctx);
+    request.packet.sender = pnzr01581;
+    request.packet.originator = pnzr01581;
+
+    request.portAddress = SERVICE_ADDRESS;
+    HttpTransportPipe.dump = true;
+
+    ClientResponse response = rev111111.send(client.getWsdlUrl(), request, null);
+
+    client.processClientResponse(response, ctx);
+
+    log.info("==== END ====");
+
+  }
+
+}
