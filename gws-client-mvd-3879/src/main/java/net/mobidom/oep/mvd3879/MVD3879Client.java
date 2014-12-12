abe8ee8f9f528aec10c23c0a2e89988e320aff16
Evgin <evgin.goncharov@gmail.com>
2014-12-12 12:01:29 +0300

    - заглушка для клиента 3879

---
+++ gws-client-mvd-3879/src/main/java/net/mobidom/oep/mvd3879/MVD3879Client.java
@@ -0,0 +1,50 @@
+package net.mobidom.oep.mvd3879;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.logging.Logger;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.namespace.QName;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import ru.codeinside.gws.api.Client;
+import ru.codeinside.gws.api.ClientRequest;
+import ru.codeinside.gws.api.ClientResponse;
+import ru.codeinside.gws.api.ExchangeContext;
+import ru.codeinside.gws.api.InfoSystem;
+import ru.codeinside.gws.api.Packet;
+import ru.codeinside.gws.api.Revision;
+import ru.codeinside.gws.api.XmlTypes;
+
+public class MVD3879Client implements Client {
+
+	static Logger log = Logger.getLogger(MVD3879Client.class.getName());
+
+	@Override
+	public Revision getRevision() {
+		return Revision.rev120315;
+	}
+
+	@Override
+	public URL getWsdlUrl() {
+		// TODO
+		return getClass().getClassLoader().getResource("mvd3879/?????.wsdl");
+	}
+
+	@Override
+	public ClientRequest createClientRequest(ExchangeContext ctx) {
+		return null;
+	}
+
+	private String createAppData(ExchangeContext ctx) {
+		return null;
+	}
+
+	@Override
+	public void processClientResponse(ClientResponse clientResponse, ExchangeContext ctx) {
+	}
+}
