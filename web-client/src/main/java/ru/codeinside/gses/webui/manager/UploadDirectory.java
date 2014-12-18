2c881af0ad44a6008b86f485eafac256747fe382
Evgin <evgin.goncharov@gmail.com>
2014-12-18 18:05:25 +0300

    загрузка справочника

--- web-client/src/main/java/ru/codeinside/gses/webui/manager/UploadDirectory.java
+++ web-client/src/main/java/ru/codeinside/gses/webui/manager/UploadDirectory.java
@@ -7,80 +7,96 @@
 
 package ru.codeinside.gses.webui.manager;
 
-import au.com.bytecode.opencsv.CSVReader;
-import com.vaadin.ui.Table;
-import com.vaadin.ui.Upload;
-import com.vaadin.ui.Window;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.tepi.filtertable.FilterTable;
+
 import ru.codeinside.adm.AdminServiceProvider;
 import ru.codeinside.gses.beans.DirectoryBeanProvider;
 import ru.codeinside.gses.webui.Flash;
+import au.com.bytecode.opencsv.CSVReader;
 
-import java.io.*;
-import java.util.Arrays;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.Upload;
+import com.vaadin.ui.Window;
 
 public class UploadDirectory implements Upload.Receiver, Upload.FinishedListener {
 
-    private static final long serialVersionUID = 11323324231L;
+  private static final long serialVersionUID = 11323324231L;
 
-    byte[] csvData;
+  static Logger log = Logger.getLogger(UploadDirectory.class.getName());
 
-    final FilterTable directoryTable;
-    final Table dirMapTable;
+  byte[] csvData;
 
-    public UploadDirectory(FilterTable directoryTable, Table dirMapTable) {
-        this.directoryTable = directoryTable;
-        this.dirMapTable = dirMapTable;
-    }
+  final FilterTable directoryTable;
+  final Table dirMapTable;
 
-    @Override
-    public OutputStream receiveUpload(String filename, String mimeType) {
-        return new ByteArrayOutputStream() {
-            @Override
-            public void close() throws IOException {
-                csvData = Arrays.copyOf(buf, count);
-            }
-        };
-    }
+  public UploadDirectory(FilterTable directoryTable, Table dirMapTable) {
+    this.directoryTable = directoryTable;
+    this.dirMapTable = dirMapTable;
+  }
 
-    @Override
-    public void uploadFinished(Upload.FinishedEvent finishedEvent) {
-        CSVReader csvReader = null;
+  @Override
+  public OutputStream receiveUpload(String filename, String mimeType) {
+    return new ByteArrayOutputStream() {
+      @Override
+      public void close() throws IOException {
+        csvData = Arrays.copyOf(buf, count);
+      }
+    };
+  }
+
+  @Override
+  public void uploadFinished(Upload.FinishedEvent finishedEvent) {
+    CSVReader csvReader = null;
+    try {
+      csvReader = new CSVReader(new InputStreamReader(new ByteArrayInputStream(csvData), "UTF-8"));
+      String[] record;
+      int lineNum = 0;
+      while ((record = csvReader.readNext()) != null) {
+        lineNum++;
+        if (record.length != 3) {
+          finishedEvent
+              .getUpload()
+              .getWindow()
+              .showNotification("Должен быть csv файл с разделителем ','. Проверьте данные, строка: " + lineNum,
+                  Window.Notification.TYPE_ERROR_MESSAGE);
+          log.log(Level.SEVERE, "can't parse cvs file, err-line:  " + lineNum);
+          return;
+        }
+        String dirName = record[0].trim();
+        DirectoryBeanProvider.get().create(dirName);
+        AdminServiceProvider.get().createLog(Flash.getActor(), "Directory", dirName, "create", "Upload create", true);
+        String key = record[1].trim();
+        String value = record[2].trim();
+        DirectoryBeanProvider.get().add(dirName, key, value);
+        AdminServiceProvider.get().createLog(Flash.getActor(), "Directory value", dirName, "add",
+            "key => ".concat(key).concat("value =>").concat(value), true);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+      return;
+    } finally {
+      if (csvReader != null) {
         try {
-            csvReader = new CSVReader(new InputStreamReader(new ByteArrayInputStream(csvData), "UTF-8"));
-            String[] record;
-            while ((record = csvReader.readNext()) != null) {
-                if (record.length != 3) {
-                    finishedEvent.getUpload().getWindow().showNotification("Должен быть csv файл с разделителем ','", Window.Notification.TYPE_ERROR_MESSAGE);
-                    return;
-                }
-                String dirName = record[0].trim();
-                DirectoryBeanProvider.get().create(dirName);
-                AdminServiceProvider.get().createLog(Flash.getActor(), "Directory", dirName, "create", "Upload create",
-                                                     true);
-                String key = record[1].trim();
-                String value = record[2].trim();
-                DirectoryBeanProvider.get().add(dirName, key, value);
-                AdminServiceProvider.get().createLog(Flash.getActor(), "Directory value", dirName, "add",
-                                                     "key => ".concat(key).concat("value =>").concat(value), true);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return;
-        } finally {
-            if (csvReader != null) {
-                try {
-                    csvReader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+          csvReader.close();
+        } catch (IOException e) {
+          e.printStackTrace();
         }
-        ManagerWorkplace.refreshDirectoryTable(directoryTable);
-        Object rowId = directoryTable.getValue();
-        if (rowId != null) {
-            final String dirName = (String) directoryTable.getContainerProperty(rowId, "name").getValue();
-            ManagerWorkplace.reloadMap(dirName, dirMapTable);
-        }
+      }
     }
+    ManagerWorkplace.refreshDirectoryTable(directoryTable);
+    Object rowId = directoryTable.getValue();
+    if (rowId != null) {
+      final String dirName = (String) directoryTable.getContainerProperty(rowId, "name").getValue();
+      ManagerWorkplace.reloadMap(dirName, dirMapTable);
+    }
+  }
 }
