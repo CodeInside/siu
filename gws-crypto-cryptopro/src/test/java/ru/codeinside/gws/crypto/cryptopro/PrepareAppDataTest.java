21aa4d2bd772abfd0509d2705b3eb23e58563011
Gnusin Pavel <pavel.gnusin@codeinside.ru>
2015-04-20 10:21:06 +0300

    re #1309 - улучшен assert в тесте

--- gws-crypto-cryptopro/src/test/java/ru/codeinside/gws/crypto/cryptopro/PrepareAppDataTest.java
+++ gws-crypto-cryptopro/src/test/java/ru/codeinside/gws/crypto/cryptopro/PrepareAppDataTest.java
@@ -9,12 +9,15 @@ public class PrepareAppDataTest extends Assert {
     @Test
     public void test_prepareData() throws IOException {
         String sourceXML = "<rev:createRequestBean xmlns:rev=\"http://smev.gosuslugi.ru/rev110801\"\n" +
-                "      ><rev:okato>11111111111</rev:okato\n" +
+                "      ><Hello/><rev:okato>11111111111</rev:okato\n" +
                 "      ><rev:requestType>558102100000</rev:requestType\n" +
                 "></rev:createRequestBean>";
         CryptoProvider cryptoProvider = new CryptoProvider();
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         cryptoProvider.prepareAppDataToSign(sourceXML, os);
-        assertTrue(os.toByteArray().length > 0);
+        String expected = "<rev:createRequestBean xmlns:rev=\"http://smev.gosuslugi.ru/rev110801\">" +
+                "<Hello></Hello><rev:okato>11111111111</rev:okato><rev:requestType>558102100000</rev:requestType>" +
+                "</rev:createRequestBean>";
+        assertEquals(expected, new String(os.toByteArray()));
     }
 }
