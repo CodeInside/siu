94d52b2e02de58159e2d4e19f78aedc37eb8bd3b
Кирилл Васин <kirill.vasin@codeinside.ru>
2015-05-05 18:04:49 +0300

    re #1327 - Формирование и подпись AppData getExecution

--- web-client/src/main/java/ru/codeinside/gses/webui/form/GetAppDataAction.java
+++ web-client/src/main/java/ru/codeinside/gses/webui/form/GetAppDataAction.java
@@ -1,7 +1,20 @@
 package ru.codeinside.gses.webui.form;
 
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.impl.ServiceImpl;
+import org.activiti.engine.impl.context.Context;
+import org.activiti.engine.impl.interceptor.Command;
+import org.activiti.engine.impl.interceptor.CommandContext;
+import org.activiti.engine.impl.interceptor.CommandExecutor;
+import org.osgi.framework.BundleContext;
 import ru.codeinside.adm.AdminServiceProvider;
 import ru.codeinside.adm.database.InfoSystemService;
+import ru.codeinside.gses.beans.ActivitiExchangeContext;
+import ru.codeinside.gses.service.F1;
+import ru.codeinside.gses.service.Fn;
+import ru.codeinside.gses.webui.Flash;
+import ru.codeinside.gses.webui.osgi.Activator;
 import ru.codeinside.gses.webui.wizard.ResultTransition;
 import ru.codeinside.gses.webui.wizard.TransitionAction;
 import ru.codeinside.gws.api.Client;
@@ -25,7 +38,11 @@ public class GetAppDataAction implements TransitionAction {
     InfoSystemService service = validateAndGetService(serviceName);
     Client client = AdminServiceProvider.get().getClientRefByNameAndVersion(service.getName(), service.getSversion()).getRef();
 
-    ClientRequest request = client.createClientRequest(null); //TODO получить ExchangeContext
+    final BundleContext context = Activator.getContext();// TODO сделать unget
+
+    DelegateExecution execution = Fn.withEngine(new GetExecution(), Flash.login());
+    ClientRequest request = client.createClientRequest(new ActivitiExchangeContext(execution));
+
     return new ResultTransition(request.appData);
   }
 
@@ -48,6 +65,31 @@ public class GetAppDataAction implements TransitionAction {
       throw new IllegalStateException("Нет модуля потребителя СМЭВ с именем '" + serviceName + "'");
     }
     return getServiceWithMaxVersion(services);
+  }
+
+  final private static class GetExecution implements F1<DelegateExecution, String> {
+    @Override
+    public DelegateExecution apply(ProcessEngine engine, String login) {
+
+      CommandExecutor commandExecutor = ((ServiceImpl) engine.getFormService()).getCommandExecutor();
+      DelegateExecution execution = (DelegateExecution) commandExecutor.execute(new GetExecutionCmd("processInstanceId"));// TODO получить processInstanceId
+      return execution;
+    }
+  }
+
+  final private static class GetExecutionCmd implements Command {
+    private final String processInstanceId;
+
+    GetExecutionCmd(String processInstanceId) {
+      this.processInstanceId = processInstanceId;
+    }
+
+    @Override
+    public Object execute(CommandContext commandContext) {
+      return Context.getCommandContext()
+          .getExecutionManager()
+          .findExecutionById(processInstanceId);
+    }
   }
 
 }
