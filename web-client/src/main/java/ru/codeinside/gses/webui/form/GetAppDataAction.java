91e620f2b5b528bf8bb6baf74c0d3e51678e93ea
Кирилл Васин <kirill.vasin@codeinside.ru>
2015-05-19 16:24:47 +0300

    re #1327 - Формирование и подпись AppData Get/unGet

--- web-client/src/main/java/ru/codeinside/gses/webui/form/GetAppDataAction.java
+++ web-client/src/main/java/ru/codeinside/gses/webui/form/GetAppDataAction.java
@@ -7,18 +7,19 @@ import org.activiti.engine.impl.context.Context;
 import org.activiti.engine.impl.interceptor.Command;
 import org.activiti.engine.impl.interceptor.CommandContext;
 import org.activiti.engine.impl.interceptor.CommandExecutor;
+import org.osgi.framework.ServiceReference;
 import ru.codeinside.adm.AdminServiceProvider;
 import ru.codeinside.adm.database.InfoSystemService;
 import ru.codeinside.gses.beans.ActivitiExchangeContext;
+import ru.codeinside.gses.beans.Smev;
 import ru.codeinside.gses.service.F2;
 import ru.codeinside.gses.service.Fn;
 import ru.codeinside.gses.webui.Flash;
+import ru.codeinside.gses.webui.osgi.Activator;
 import ru.codeinside.gses.webui.wizard.ResultTransition;
 import ru.codeinside.gses.webui.wizard.TransitionAction;
 import ru.codeinside.gws.api.Client;
 import ru.codeinside.gws.api.ClientRequest;
-
-import java.util.List;
 
 public class GetAppDataAction implements TransitionAction {
 
@@ -35,63 +36,50 @@ public class GetAppDataAction implements TransitionAction {
    */
   @Override
   public ResultTransition doIt() throws IllegalStateException {
-    InfoSystemService service = validateAndGetService(serviceName);
-    Client client = AdminServiceProvider.get().getClientRefByNameAndVersion(service.getName(), service.getSversion()).getRef();
+    final Smev smev = getSmev();
+    final InfoSystemService service = smev.validateAndGetService(serviceName);
+    final Client client = AdminServiceProvider.get().getClientRefByNameAndVersion(service.getName(), service.getSversion()).getRef();
     dataAccumulator.setClient(client);
 
-    ClientRequest request = Fn.withEngine(new GetClientRequest(), Flash.login(), dataAccumulator);
+    final ClientRequest request = Fn.withEngine(new GetClientRequest(), Flash.login(), dataAccumulator);
     dataAccumulator.setClientRequest(request);
 
     return new ResultTransition(request.appData);
   }
 
-  private InfoSystemService validateAndGetService(String serviceName) {
-    List<InfoSystemService> services = AdminServiceProvider.get().getInfoSystemServiceBySName(serviceName);
-    if (services == null || services.isEmpty()) {
-      throw new IllegalStateException("Нет модуля потребителя СМЭВ с именем '" + serviceName + "'");
-    }
-    return getServiceWithMaxVersion(services);
-  }
-
-  private InfoSystemService getServiceWithMaxVersion(List<InfoSystemService> services) {
-    InfoSystemService curService = null;
-    for (InfoSystemService s : services) {
-      if (curService == null) {
-        curService = s;
-      }
-      if (s.getSversion().compareTo(curService.getSversion()) >= 0) {
-        curService = s;
-      }
-    }
-    return curService;
+  private Smev getSmev() {
+    ServiceReference serviceReference = Activator.getContext().getServiceReference(Smev.class.getName());
+    Smev smev = (Smev) Activator.getContext().getService(serviceReference);
+    Activator.getContext().ungetService(serviceReference);
+    return smev;
   }
 
   final private static class GetClientRequest implements F2<ClientRequest, String, DataAccumulator> {
     @Override
     public ClientRequest apply(ProcessEngine engine, String login, DataAccumulator dataAccumulator) {
-
       CommandExecutor commandExecutor = ((ServiceImpl) engine.getFormService()).getCommandExecutor();
-      DelegateExecution execution = (DelegateExecution) commandExecutor.execute(new GetExecutionCmd(dataAccumulator.getTaskId()));
-
-      return dataAccumulator.getClient().createClientRequest(new ActivitiExchangeContext(execution));
+      return (ClientRequest) commandExecutor.execute(new GetClientRequestCmd(dataAccumulator.getTaskId(), dataAccumulator.getClient()));
     }
   }
 
-  final private static class GetExecutionCmd implements Command {
+  final private static class GetClientRequestCmd implements Command {
     private final String taskId;
+    private final Client client;
 
-    GetExecutionCmd(String taskId) {
+    GetClientRequestCmd(String taskId, Client client) {
       this.taskId = taskId;
+      this.client = client;
     }
 
     @Override
     public Object execute(CommandContext commandContext) {
-      commandContext.getVariableInstanceManager().findVariableInstancesByExecutionId("");
-      String processInstanceId = AdminServiceProvider.get().getBidByTask(taskId).getProcessInstanceId();
+      final String processInstanceId = AdminServiceProvider.get().getBidByTask(taskId).getProcessInstanceId();
 
-      return Context.getCommandContext()
+      DelegateExecution execution = Context.getCommandContext()
           .getExecutionManager()
           .findExecutionById(processInstanceId);
+
+      return client.createClientRequest(new ActivitiExchangeContext(execution));
     }
   }
 }
